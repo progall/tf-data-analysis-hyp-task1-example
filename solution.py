082b2a6b688c1bf2a81966300a0be4dddf724b1a
progall <progall@users.noreply.github.com>
2023-04-11 16:30:49 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import ttest_ind
+from scipy.stats import pareto, cauchy, norm, ttest_ind, ks_2samp, mannwhitneyu, permutation_test
 
 chat_id = 322172960 # Ваш chat ID, не меняйте название переменной
 
@@ -12,4 +12,5 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ttest_ind(x_success/x_cnt, y_success/y_cnt, equal_var=False, alternative="greater").pvalue > 0.04 # Ваш ответ, True или False
+    #return ttest_ind(x_success/x_cnt, y_success/y_cnt, equal_var=False, alternative="greater").pvalue < 0.04 # Ваш ответ, True или False
+    return mannwhitneyu(x_success/x_cnt, y_success/y_cnt, alternative="less").pvalue < 0.04 # Ваш ответ, True или False
